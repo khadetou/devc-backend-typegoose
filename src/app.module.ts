eebd452128184236bd7303181d76594137f3a496
khadetou <khadetou96@gmail.com>
2022-01-22 20:10:10 +0000

    typegoose config and installation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
-
 import { AuthModule } from './auth/auth.module';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
+import { TypegooseModule } from 'nestjs-typegoose';
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`, '.env'],
+      validationSchema: configValidationSchema,
+    }),
+    TypegooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGO_URI'),
+        useNewUrlParser: true,
+      }),
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
